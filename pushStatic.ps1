b4abec37f40dd2745105baa7cb217bee7fe770dc
johnnyreilly <johnny_reilly@hotmail.com>
2015-07-12 21:28:56 +0100

    Copy build into static site folder first

--- pushStatic.ps1
+++ pushStatic.ps1
@@ -5,6 +5,9 @@ git config --global user.email $email
 git config --global user.name $username
 git config --global push.default matching
 
+Write-Host "- Copy contents of build folder into a static-site folder...."
+copy-item -path .\build\* -Destination ..\static-site\* -Recurse
+
 Write-Host "- Clone gh-pages branch...."
 cd "$($buildFolder)\..\"
 mkdir gh-pages
